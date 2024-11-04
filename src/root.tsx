3e5ec76e85fe9ecb9e68e54438c588a0a97042df
Rui Vilela <tek@qwertytek.com>
2024-11-04 03:53:58 +0000

    minor style change

--- src/root.tsx
+++ src/root.tsx
@@ -29,7 +29,7 @@ export default component$(() => {
         )}
         <RouterHead />
       </head>
-      <body class="p-10" lang="en">
+      <body lang="en">
         <RouterOutlet />
         {!isDev && <ServiceWorkerRegister />}
       </body>
