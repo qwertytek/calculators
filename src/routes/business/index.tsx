9bb9ce483c3db58f0cdc1c1960131dc56ed5a24b
Rui Vilela <tek@qwertytek.com>
2024-11-04 04:24:23 +0000

    added popup

--- src/routes/business/index.tsx
+++ src/routes/business/index.tsx
@@ -1,8 +1,7 @@
 import { component$, useStore, useComputed$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Input } from "~/components/ui";
-import { Label } from "~/components/ui";
-import { Breadcrumb } from '~/components/ui';
+import { LuX } from "@qwikest/icons/lucide";
+import { Input, Breadcrumb, Modal, Label, buttonVariants } from "~/components/ui";
 
 export default component$(() => {
   const state = useStore({
@@ -29,42 +28,61 @@ export default component$(() => {
       </Breadcrumb.Root>
       <h1 class="mt-8">Business Calculator</h1>
       <p class="mb-6 mt-1">
-        A bunch of calculators to better understand if your business idea makes mathematical sense
+        A bunch of calculators to better understand if your business idea makes
+        mathematical sense
       </p>
-      <div class="w-1/3">
-        <Label for="subscription-cost-input"> Monthly Subscription costs</Label>
-        <Input
-          class="mb-8 w-1/3"
-          value={state.subscription}
-          id="subscription-cost-input"
-          min={0}
-          step={1}
-          pattern="^[0-9]+$"
-          type="number"
-          onInput$={(_, el) => (state.subscription = Number(el.value))}
-        />
-        <Label for="clients-input"> Number of Clients </Label>
-        <Input
-          class="w-1/3"
-          id="clients-input"
-          type="number"
-          value={state.clients}
-          min={0}
-          step={1}
-          pattern="^[0-9]+$"
-          onInput$={(_, el) => (state.clients = Number(el.value))}
-        />
-      </div>
-      <div class="mt-10">
-        <div>
-          <h2>Monthly Revenue</h2>
-          <p> {monthlyRevenue.value} $</p>
-        </div>
-        <div>
-          <h2>Yearly Revenue</h2>
-          <p>{Number(monthlyRevenue.value) * 12} $</p>
-        </div>
-      </div>
+      <Modal.Root>
+        <Modal.Trigger class={[buttonVariants({ look: 'primary' })]}>
+          Revenue Calculator
+        </Modal.Trigger>
+        <Modal.Panel>
+          <Modal.Close class="absolute top-4 right-4">
+            <LuX class="h-5 w-5" />
+          </Modal.Close>
+          <Modal.Title class="text-4xl mb-5"> Revenue Calculator </Modal.Title>
+          <Modal.Description class="mb-5">
+            Know how many clients and how much you should charge for your subscription service
+          </Modal.Description>
+          <>
+            <div>
+              <Label for="subscription-cost-input">
+                Monthly Subscription costs
+              </Label>
+              <Input
+                class="mb-8 w-1/3"
+                value={state.subscription}
+                id="subscription-cost-input"
+                min={0}
+                step={1}
+                pattern="^[0-9]+$"
+                type="number"
+                onInput$={(_, el) => (state.subscription = Number(el.value))}
+              />
+              <Label for="clients-input"> Number of Clients </Label>
+              <Input
+                id="clients-input"
+                class="w-1/3"
+                type="number"
+                value={state.clients}
+                min={0}
+                step={1}
+                pattern="^[0-9]+$"
+                onInput$={(_, el) => (state.clients = Number(el.value))}
+              />
+            </div>
+            <div class="mt-10 flex justify-between text-center">
+              <div>
+                <h2>Monthly Revenue</h2>
+                <p> {monthlyRevenue.value} $</p>
+              </div>
+              <div>
+                <h2>Yearly Revenue</h2>
+                <p>{Number(monthlyRevenue.value) * 12} $</p>
+              </div>
+            </div>
+          </>
+        </Modal.Panel>
+      </Modal.Root>
     </>
   );
 });
