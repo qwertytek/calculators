114d1054bf13e1edbed5abd33971213cc4da4a11
Rui Vilela <tek@qwertytek.com>
2024-11-04 02:20:35 +0000

    reverted tailwind installation

--- src/routes/revenue/index.tsx
+++ src/routes/revenue/index.tsx
@@ -1,6 +1,5 @@
 import { component$, useStore, useComputed$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   const state = useStore({
@@ -12,7 +11,6 @@ export default component$(() => {
 
   return (
     <>
-      <Link href="/"> Home </Link>
       <h1>Business Calculator 👋</h1>
       <p>
         A calculator to visualize how much you should charge on you subscription
